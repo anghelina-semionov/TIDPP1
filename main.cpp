eeff5ea40ee8dce2fd0c596b9d19fa4e87835f10
Anghelina Semionov <anghelina.semionov@isa.utm.md>
2020-09-08 18:32:12 +0300

    Добавила main.cpp в newbranch-7

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>  
+
+using namespace std;
+
+
+int main()
+{
+
+    int ans;
+
+    cout << "125 : 5 = ";
+    cin >> ans;
+    cout << endl;
+    if (ans == 25) cout << "Right!";
+    else cout << "Wrong!";
+    return 0;
+}
