d861a6873486fcccd05c0e56b06fe1e8d5e87b1c
Anghelina Semionov <anghelina.semionov@isa.utm.md>
2020-09-08 18:49:23 +0300

    Добавила main.cpp в newbranch-10

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>  
+
+using namespace std;
+
+
+int main()
+{
+
+    string a;
+
+    cout << "What's your name?" << endl;
+    cin >> a;
+    cout << "Nice to meet you, " << a << ".";
+
+    return 0;
+}
